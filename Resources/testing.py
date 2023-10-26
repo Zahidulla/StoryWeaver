efe1a45cf7e576fe7be0255f620404df541dd6f8
Zahidulla <88815355+Zahidulla@users.noreply.github.com>
2023-10-26 22:39:58 +0530

    Update testing.py

--- Resources/testing.py
+++ Resources/testing.py
@@ -3,8 +3,12 @@ from selenium.webdriver.chrome.options import Options
 
 
 def chromedriver(Url):
-    options = Options()
-    options.page_load_strategy = 'normal'
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome(options=options)
     driver.get(Url)
     driver.quit()
