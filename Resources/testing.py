14d5fd1a41c6e3ab56fb329bdc414763677c347d
Zahidulla <zahidulla201@gmail.com>
2023-10-27 10:35:27 +0530

    Merge remote-tracking branch 'origin/master'

--- Resources/testing.py
+++ Resources/testing.py
@@ -3,8 +3,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 def chromedriver(Url):
-    options = Options()
-    options.page_load_strategy = 'normal'
+    browser_path = '/usr/bin/google-chrome'
+    options = webdriver.ChromeOptions()
+    options.binary_location = browser_path
+    options.add_argument('headless')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome(options=options)
     driver.get(Url)
     driver.quit()
